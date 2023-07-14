4342e6869fd8478e4316d07bf2dce2728092a8bf
metahead00 <124059438+metahead00@users.noreply.github.com>
2023-07-14 19:54:27 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,62 +1,101 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <style>
-    canvas {
-      position: fixed;
-      top: 0;
-      left: 0;
-    }
-  </style>
-</head>
-<body>
-  <canvas id="fireworks-canvas"></canvas>
+const text = document.getElementById("greeting");
+const letters = text.textContent.split("");
 
-  <script>
-    const canvas = document.getElementById('fireworks-canvas');
-    const ctx = canvas.getContext('2d');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+text.innerHTML = ""; // Clear the original text
 
-    function createFirework() {
-      const x = Math.random() * canvas.width;
-      const y = canvas.height;
-      const color = `hsl(${Math.random() * 360}, 100%, 50%)`;
-      const sparkCount = 100;
+letters.forEach((letter) => {
+  const span = document.createElement("span");
+  span.textContent = letter;
+  text.appendChild(span);
+});
 
-      for (let i = 0; i < sparkCount; i++) {
-        const angle = Math.random() * Math.PI * 2;
-        const velocity = Math.random() * 4 + 2;
-        const sparkX = x;
-        const sparkY = y;
-        const gravity = 0.1;
-        const opacity = 1;
+text.addEventListener("mouseenter", () => {
+  const spans = text.getElementsByTagName("span");
 
-        setInterval(() => {
-          ctx.fillStyle = color;
-          ctx.beginPath();
-          ctx.arc(sparkX, sparkY, 2, 0, Math.PI * 2);
-          ctx.closePath();
-          ctx.fill();
+  for (let i = 0; i < spans.length; i++) {
+    const randomColor = getRandomColor();
+    const randomFont = getRandomFont();
 
-          sparkX += Math.cos(angle) * velocity;
-          sparkY += Math.sin(angle) * velocity;
-          velocity *= 0.98;
-          velocity -= gravity;
-          opacity -= 0.01;
+    spans[i].style.color = randomColor;
+    spans[i].style.fontFamily = randomFont;
+  }
+});
 
-          if (opacity <= 0) clearInterval();
-        }, 10);
-      }
-    }
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
 
-    function updateFireworks() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      createFirework();
-      requestAnimationFrame(updateFireworks);
-    }
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
 
-    updateFireworks();
-  </script>
-</body>
-</html>
+  return color;
+}
+
+function getRandomFont() {
+  const fonts = ["Arial", "Helvetica", "Times New Roman", "Courier New", "Verdana", "Georgia"];
+  const randomIndex = Math.floor(Math.random() * fonts.length);
+  
+  return fonts[randomIndex];
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const maze = document.getElementById('maze');
+    const player = document.createElement('div');
+    const goal = document.createElement('div');
+
+    player.classList.add('player');
+    goal.classList.add('goal');
+
+    maze.appendChild(player);
+    maze.appendChild(goal);
+
+    const movePlayer = function(event) {
+        const key = event.keyCode;
+        const currentPosition = {
+            top: player.offsetTop,
+            left: player.offsetLeft
+        };
+
+        switch (key) {
+            case 37: // left
+                if (currentPosition.left > 0) {
+                    player.style.left = (currentPosition.left - 40) + 'px';
+                }
+                break;
+            case 38: // up
+                if (currentPosition.top > 0) {
+                    player.style.top = (currentPosition.top - 40) + 'px';
+                }
+                break;
+            case 39: // right
+                if (currentPosition.left < maze.offsetWidth - 80) {
+                    player.style.left = (currentPosition.left + 40) + 'px';
+                }
+                break;
+            case 40: // down
+                if (currentPosition.top < maze.offsetHeight - 80) {
+                    player.style.top = (currentPosition.top + 40) + 'px';
+                }
+                break;
+        }
+
+        if (checkCollision(player, goal)) {
+            alert('CONGRATS!');
+        }
+    };
+
+    const checkCollision = function(element1, element2) {
+        const rect1 = element1.getBoundingClientRect();
+        const rect2 = element2.getBoundingClientRect();
+
+        return (
+            rect1.top < rect2.bottom &&
+            rect1.bottom > rect2.top &&
+            rect1.left < rect2.right &&
+            rect1.right > rect2.left
+        );
+    };
+
+    document.addEventListener('keydown', movePlayer);
+});
